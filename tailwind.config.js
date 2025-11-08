a0367a6e5495b445ba153445c2084c185f0c4306
Souid83 <swuidy@hotmail.fr>
2025-11-08 23:28:41 +0100

    q

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#13ecda',
+        'background-light': '#f6f8f8',
+        'background-dark': '#102220',
+      },
+      fontFamily: {
+        display: ['Inter', 'sans-serif'],
+      },
+      borderRadius: {
+        DEFAULT: '0.5rem',
+        lg: '1rem',
+        xl: '1.5rem',
+        full: '9999px',
+      },
+    },
   },
   plugins: [],
 };
